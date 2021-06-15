e2a676248f6d49a8db8f608d745b54cc240c4918
Martín Rodríguez <neomartinr@gmail.com>
2021-06-15 17:36:34 -0300

    la

--- js/arrays.js
+++ js/arrays.js
@@ -5,8 +5,7 @@ console.log(array);
 array.push("Laura");
 array.push("Luciano");
 array.push("Tomas");
-array[0] = 11;
-
+array[0] = "Jose";
 // array = ["Algo", "y algo mas"]
 console.log(array);
 // console.log(array);
@@ -17,6 +16,8 @@ console.log(array);
 console.log('Longitud del array: ', array.length);
 
 for(let i=0; i < array.length; i++) {
+    // i
+
     document.write(array[i] + '<br>')
                         //18
 }
