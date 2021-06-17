120705eabea7a7b622f1535dd6865dd00d630eee
Martín Rodríguez <neomartinr@gmail.com>
2021-06-17 15:41:27 -0300

    ASync for and functions

--- js/03-arrays-dados.js
+++ js/03-arrays-dados.js
@@ -6,19 +6,25 @@ let maximo = 0;
 let jugadores = ["Carla", "María", "Juan", "Jose", "Pedro"];
 // Obtengo los números correctos de dados entre 1 y 6
 // Math.trunc((Math.random()) * 6) + 1
-
+let button = document.querySelectorAll('button');
+console.log(button);
 
 //i hace referencia a las rondas de tiros
 for(let i = 0; i < 10; i++) {
     //Hace referencia a cada jugador
     //i = 
-   
+    
     for(let j = 0; j < jugadores.length; j++) {
         //***** i=0 *
-        if(resultados[j] == undefined) {
-            console.log('Entra al if')
-            resultados[j] = 0;
-        }
+        console.log(button[j].classList);
+
+        if(button[j].classList.contains('disabled')) button[j].classList.remove('disabled');
+        // if(button[j].classList.add('disabled'));
+        (function(j) {
+            setTimeout(() =>  {
+                console.log('This j', j);
+            }, 2000)
+        })
         //j = 1
         let dadoUno = Math.trunc((Math.random()) * 6) + 1;
         let dadoDos = Math.trunc((Math.random()) * 6) + 1;
@@ -37,8 +43,12 @@ for(let i = 0; i < 10; i++) {
     // suma = suma + dadoUno + dadoDos;
     // console.log('SUMA', suma);
 }    
-console.log('Resultados', resultados);            
 
+
+
+
+console.log('Resultados', resultados);   
+                //0
 for(let i = 0; i< jugadores.length; i++) {
     console.log(`El jugador ${jugadores[i]} obtuvo ${resultados[i]}`);
         //0
@@ -49,3 +59,9 @@ for(let i = 0; i< jugadores.length; i++) {
 }
 
 console.log(`El ganador es ${ganador} con un puntaje de ${maximo}`);
+
+async function tirarDados(player){
+    let dadoUno = Math.trunc((Math.random()) * 6) + 1;
+    let dadoDos = Math.trunc((Math.random()) * 6) + 1;
+    return alert(`El resultado obtenido por ${jugadores[player]}: ${ dadoUno + dadoDos } (${dadoDos} + ${dadoUno})`);
+}
