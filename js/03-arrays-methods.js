8e869ff15477bb45aad8a04ae6791b2d654320f7
Martín Rodríguez <neomartinr@gmail.com>
2021-06-17 19:08:35 -0300

    Fixed America cup duplicated name

--- js/03-arrays-methods.js
+++ js/03-arrays-methods.js
@@ -1,3 +1,3 @@
 let copa = [
-  Brasil,Chile,Chile,Uruguay,Brasil,Brasil,Colombia,Brasil,Brasil,Uruguay,Argentina,Argentina,Brasil,Uruguay,Uruguay,Uruguay,Paraguay,PerúPerú,Uruguay,Bolivia,Uruguay,Argentina,Argentina,Uruguay,Argentina,Paraguay,Brasil,Argentina,Argentina,Argentina,Uruguay,Argentina,PerúPerú,Argentina,Uruguay,Argentina,Argentina,Uruguay,Argentina,Uruguay,Uruguay,Brasil,Argentina,Uruguay,Brasil,Uruguay,Uruguay,
+  Brasil,Chile,Chile,Uruguay,Brasil,Brasil,Colombia,Brasil,Brasil,Uruguay,Argentina,Argentina,Brasil,Uruguay,Uruguay,Uruguay,Paraguay,Perú,Uruguay,Bolivia,Uruguay,Argentina,Argentina,Uruguay,Argentina,Paraguay,Brasil,Argentina,Argentina,Argentina,Uruguay,Argentina,Perú,Argentina,Uruguay,Argentina,Argentina,Uruguay,Argentina,Uruguay,Uruguay,Brasil,Argentina,Uruguay,Brasil,Uruguay,Uruguay,
 ];
